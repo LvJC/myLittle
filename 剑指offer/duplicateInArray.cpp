0ca7b9939709cff021a3c45d7b3743070c0c1d98
LvJC <lvjincheng@tju.edu.cn>
2019-07-26 00:40:03 +0800

    Create duplicateInArray.cpp
    剑指offer 练习1

---
+++ 剑指offer/duplicateInArray.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int duplicateInArray(vector<int>& nums) {
+        int n = nums.size();
+        if (n==0)    return -1;
+        //先遍历一遍，看是否有超出范围的数
+        for (int i = 0; i < n; i++){
+            if (nums[i]<0 || nums[i]>n-1)   return -1;
+        }
+        //交换的判断条件：
+        //1. nums[i]不在原来的位置
+        //2. nums[nums[i]]和nums[i]不相等
+        for (int i = 0; i < n; i++){
+            int loc = nums[i];
+            if (i!=loc && nums[loc]!=loc)
+                swap(nums[i], nums[loc]);
+            else if (i!=loc && nums[loc]==loc)
+                return loc;
+        }
+        return -1;
+    }
+};
